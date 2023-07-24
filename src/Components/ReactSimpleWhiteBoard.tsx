b908377fb7f8307129e3dd9a804bec971869c65c
Jonathan Polina <69684393+jpolina@users.noreply.github.com>
2023-07-24 00:01:02 -0400

    Make image background white

--- src/Components/ReactSimpleWhiteBoard.tsx
+++ src/Components/ReactSimpleWhiteBoard.tsx
@@ -22,12 +22,19 @@ const ReactSimpleWhiteBoard = React.forwardRef<HTMLCanvasElement>((props: ReactS
     );
 
     setCanvasWidth(width);
+    
+    
   }, []);
 
   useEffect(() => {
     if (!canvasRef.current) return;
 
     whiteBoard.current = new SimpleWhiteBoard(canvasRef.current);
+
+    const canvas = canvasRef.current;
+    const ctx = canvasRef.current?.getContext('2d');
+    ctx!.fillStyle = 'white';
+    ctx!.fillRect(0, 0, canvas!.width, canvas!.height);
 
     return () => {
       if (whiteBoard.current) {
@@ -42,6 +49,25 @@ const ReactSimpleWhiteBoard = React.forwardRef<HTMLCanvasElement>((props: ReactS
     whiteBoard.current.setLineColor(lineColor);
     whiteBoard.current.setLineWidth(lineWidth);
   }, [lineColor, lineWidth]);
+
+
+  const saveImage = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvasRef.current?.getContext('2d');
+    if (canvas != null && ctx != null) {
+      let dataURL = canvas.toDataURL("image/png", 1.0);
+      downloadImage(dataURL, 'my-canvas.png');
+    }
+  }
+
+  function downloadImage(data: any, filename = 'untitled.png') {
+    let a = document.createElement('a');
+    a.href = data;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+  }
+
 
   return (
     <div className="react-simple-white-board">
@@ -59,6 +85,9 @@ const ReactSimpleWhiteBoard = React.forwardRef<HTMLCanvasElement>((props: ReactS
       />
       <div>
         <button onClick={() => whiteBoard.current?.erase()}>Clear</button>
+      </div>
+      <div>
+        <button onClick={saveImage}>Get image</button>
       </div>
     </div>
   );
