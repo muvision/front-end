c77e4a7707f01228b02c36e70a8706a08bbef409
Jonathan Polina <69684393+jpolina@users.noreply.github.com>
2023-08-04 12:06:34 -0400

    Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'theme':'#7C7EAE',
+      'theme-stroke':"#5D5F88"
+    },
+    fontFamily: {
+      'sans': ['Chakra Petch', 'sans-serif'],
+      'serif': ['Roboto Slab', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
