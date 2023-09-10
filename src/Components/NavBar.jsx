a1510d23c1eea84e7f9505bcc14082ee2decc3c7
Jonathan Polina <69684393+jpolina@users.noreply.github.com>
2023-09-09 21:49:04 -0400

    Switch home href to demo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -79,7 +79,7 @@ export default function Navbar() {
           <a href="#" className="text-xl font-semibold leading-6 text-gray-900 hover:text-theme hover:scale-110 transform transition ease-in-out duration-150">
             HOME
           </a>
-          <a href="#home" className="text-xl font-semibold leading-6 text-gray-900 hover:text-theme hover:scale-110 transform transition ease-in-out duration-150">
+          <a href="#demo" className="text-xl font-semibold leading-6 text-gray-900 hover:text-theme hover:scale-110 transform transition ease-in-out duration-150">
             DEMO
           </a>
           <a href="#howitworks" className="text-xl font-semibold leading-6 text-gray-900 hover:text-theme hover:scale-110 transform transition ease-in-out duration-150">
@@ -126,7 +126,7 @@ export default function Navbar() {
                   HOME
                 </a>
                 <a
-                  href="#home"
+                  href="#demo"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   onClick={() => setMobileMenuOpen(false)}
                 >
--- src/container/WhiteBoard.jsx
+++ src/container/WhiteBoard.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export const WhiteBoard = () => {
   return (
-    <div id="home">
+    <div id="demo">
         <ReactSimpleWhiteBoard/>
     </div>
   )
